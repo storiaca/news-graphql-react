501a43b597ab6baa4ef47cdac551ccac07617d1b
Aleksandar Ristic <storiaca@gmail.com>
2019-12-13 15:55:28 +0100

    Fix Story.js component

--- src/components/Story.js
+++ src/components/Story.js
@@ -10,6 +10,7 @@ import {
 } from "../styles/StoryStyles";
 export const Story = memo(function Story({ storyId }) {
   const [story, setStory] = useState({});
+
   useEffect(() => {
     getStory(storyId).then(data => data && data.url && setStory(data));
   }, []);
@@ -21,15 +22,13 @@ export const Story = memo(function Story({ storyId }) {
       </StoryTitle>
       <StoryMeta>
         <span data-testid="story-by">
-          <StoryMetaElement color="#000">By: </StoryMetaElement>
-          {story.by}
+          <StoryMetaElement color="#000">By:</StoryMetaElement> {story.by}
         </span>
         <span data-testid="story-time">
-          <StoryMetaElement color="#000">Posted: </StoryMetaElement>
+          <StoryMetaElement color="#000">Posted:</StoryMetaElement> {` `}
           {mapTime(story.time)}
         </span>
       </StoryMeta>
     </StoryWrapper>
   ) : null;
-}
-
+});
